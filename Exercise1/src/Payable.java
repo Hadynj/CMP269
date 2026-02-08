135278ef06ceeb73ad58859f5847338442109819
hadynj <hadynjiee@gmail.com>
2026-02-07 23:21:46 -0500

    The Interface: Payable

--- Exercise1/src/Payable.java
+++ Exercise1/src/Payable.java
@@ -1,3 +1,9 @@
+/*
+    1. The Interface: Payable
+    Create an interface named Payable that defines the "contract" for any payment method.
+    Method: void processPayment(double amount)
+    Method: String getPaymentStatus()
+*/
 
 public interface Payable {
 	void processPayment(double amount);
