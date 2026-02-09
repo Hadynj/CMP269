e8f41e2e1afc824d804f51d5f28dc535f4fdd86e
hadynj <hadynjiee@gmail.com>
2026-02-09 16:32:52 -0500

    The Concrete Subclass: CreditCard

---
+++ Exercise1/src/CreditCard.java
@@ -0,0 +1,15 @@
+/*
+ 	3. The Concrete Subclass: CreditCard
+		Create a class CreditCard that extends PaymentMethod.
+		Additional Field: private double creditLimit
+		Requirement: Override processPayment.
+		    If the amount exceeds balance + creditLimit,
+		    print "Transaction Declined."
+		    Otherwise, subtract the amount and
+		    increment the totalTransactions static counter.
+		Requirement: Use the super keyword to call the parent constructor.
+
+ * */
+public abstract class CreditCard extends PaymentMethod{
+
+}
