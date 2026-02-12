d92a3dd48ba3ad558563610dcf169eee5e09c026
hadynj <hadynjiee@gmail.com>
2026-02-11 21:59:52 -0500

    The Concrete Subclass: Meal Plan

--- Exercise1/src/MealPlan.java
+++ Exercise1/src/MealPlan.java
@@ -1,8 +1,15 @@
+/*
+    4. The Concrete Subclass: MealPlan
+        Create a class MealPlan that extends PaymentMethod.
+        Requirement: Override validateAccount. Meal plan balances cannot be negative.
+        Requirement: Implement processPayment specifically for meal plan logic (e.g., only allowing payments if the balance is sufficient).
 
+* */
 public abstract class MealPlan {
 
 	public MealPlan() {
 		// TODO Auto-generated constructor stub
+        
 	}
 
 }
