05b6fbfdb6acd1ef92637332f0d80507b087de3e
hadynj <hadynjiee@gmail.com>
2026-02-25 22:25:59 -0500

    Exercise5

--- Exercise5/src/GreeterTask.java
+++ Exercise5/src/GreeterTask.java
@@ -5,4 +5,29 @@ public class GreeterTask implements Runnable{
 		// TODO Auto-generated constructor stub
 	}
 
+    @Override
+    public void run(){
+        for (int i = 1; i <= 5; i++){
+            System.out.println(""
+            + Thread.currentThread().getName);
+            try{
+                Thread.sleep(500);
+            } catch (InterrptedException e){
+                e.printStacktrace();
+            }
+        }
+    }
+
+}
+
+public class GreetingThreads{
+    public static void main(String[] args){
+        GreeterTask task = new GreeterTask();
+
+        Thread t1 = new Thread(task, "Lehman-Thread-1");
+        Thread t2 = new Thread(task, "Lehman-Thread-2");
+
+        t1.start();
+        t2.start();
+    }
 }
