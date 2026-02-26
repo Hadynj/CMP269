c341f85f5eb2c26f6690bd486e3554aac5d2a0f9
hadynj <hadynjiee@gmail.com>
2026-02-25 22:33:39 -0500

    Exercise5

--- Exercise5/src/BankAccount.java
+++ Exercise5/src/BankAccount.java
@@ -1,8 +1,22 @@
 
 public class BankAccount {
+    // fields
+    private int balance = 1000;
 
 	public BankAccount() {
 		// TODO Auto-generated constructor stub
 	}
+
+    public void withdraw(int amount){
+        if (balance >= amount){
+            System.out.println();
+            balance -= amount;
+            System.out.println();
+
+        }
+        else{
+            System.out.println();
+        }
+    }
 
 }
