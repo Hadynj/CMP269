ec6389711acb6d351eaece30f05b9c7bd0527c1e
hadynj <hadynjiee@gmail.com>
2026-04-01 21:11:57 -0400

    Exercise1

---
+++ Kotlin/Exercise4/Exercise1.gradle.kts
@@ -0,0 +1,20 @@
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import java.nio.file.FileSystems
+import java.nio.file.Path
+
+fun saveQRCode(content: String, fileName: String) {
+    val width = 300
+    val height = 300
+
+    val bitMatrix = QRCodeWriter().encode(
+        content,
+        BarcodeFormat.QR_CODE,
+        width,
+        height
+    )
+
+    val path: Path = FileSystems.getDefault().getPath(fileName)
+    MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path)
+}
