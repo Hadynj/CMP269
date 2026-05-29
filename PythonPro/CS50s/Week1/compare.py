dad85068a1c8193abd5c2d224cb118c70ffa2f5e
hadynj <hadynjiee@gmail.com>
2026-05-29 19:42:01 -0400

    Compare conditionals

--- PythonPro/CS50s/Week1/compare.py
+++ PythonPro/CS50s/Week1/compare.py
@@ -17,3 +17,19 @@ elif x > y:
     print("x is greater than y")
 else: 
     print("x is equal to y")
+
+'''conditional where there is only one equation
+
+x == y or not
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
+
+
+x is not equal to y or equal
+if x != y: 
+    print("x is not equal to y")
+else: 
+    print("x is equal to y")
+'''
---
+++ PythonPro/ThinkPy/Chapter2.py
@@ -0,0 +1,9 @@
+import math 
+
+# calculute the volume of sphere 
+radius = 5     # assign value to radius
+volume_of_sphere = 4 * math.pi * radius**3     # compute the volume of sphere 
+print(round(volume_of_sphere), 2)     # display volume of sphere
+
+
+
