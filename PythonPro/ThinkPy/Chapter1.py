f24818711e5b654597838916b03ab50580e90ac4
hadynj <hadynjiee@gmail.com>
2026-05-28 23:36:09 -0400

    The way of the program

--- PythonPro/ThinkPy/Chapter1.py
+++ PythonPro/ThinkPy/Chapter1.py
@@ -3,6 +3,10 @@ print("Hello!")
 
 
 # Exercise 1.1
-Tot_sec = (42*60) + 42         # minutes to sec 
+total_sec = (42*60) + 42         # minutes to sec 
 print(Tot_sec, "sec")          # display result
-    
+
+
+# Exercise 1.2 
+total_mile = (10/1.61)         # kilometers to miles 
+print(round(total_mile, "mile"), 4)         # display result
