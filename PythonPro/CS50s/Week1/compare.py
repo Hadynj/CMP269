c5434fae5c24356892de62467965c68c94052022
hadynj <hadynjiee@gmail.com>
2026-05-29 19:27:22 -0400

    Compare conditionals

--- PythonPro/CS50s/Week1/compare.py
+++ PythonPro/CS50s/Week1/compare.py
@@ -1,2 +1,19 @@
 x = int(input("What's x? "))      # ask user for input x 
 y = int(input("What's y? "))      # ask user for input y
+
+'''
+start 
+x < y --> true
+x is less than y 
+
+x < y --> false 
+x is greater than y
+'''
+
+# check conditional if x < y, x > y, else x == y
+if x < y:          
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else: 
+    print("x is equal to y")
