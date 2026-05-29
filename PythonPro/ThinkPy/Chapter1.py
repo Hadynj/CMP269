106d4cd34f0f9888b640ca56c363e292ef973f2f
hadynj <hadynjiee@gmail.com>
2026-05-28 23:44:31 -0400

    The way of the program

--- PythonPro/ThinkPy/Chapter1.py
+++ PythonPro/ThinkPy/Chapter1.py
@@ -4,9 +4,25 @@ print("Hello!")
 
 # Exercise 1.1
 total_sec = (42*60) + 42         # minutes to sec 
-print(Tot_sec, "sec")          # display result
+print(total_sec, "sec")          # display result
 
 
 # Exercise 1.2 
 total_mile = (10/1.61)         # kilometers to miles 
-print(round(total_mile, "mile"), 4)         # display result
+print(round(total_mile, "mile"), 4)         # display 
+
+
+# Exercise 1.3 
+total_sec = (42*60) + 42     # minutes to sec
+print(round(total_sec, "sec"), 2)      # display total sec
+print(total_mile/total_sec, "mile per sec")
+
+
+total_min = (42) + (42/60)    # sec to mins
+print(round(total_min), 2)    # display mins  
+print(total_mile/total_min, "mile per minute")    # display results
+
+
+total_hr = (42/60)+(42/3600)   
+total_hr
+print(total_mile/total_hr, "mile per hr")   
