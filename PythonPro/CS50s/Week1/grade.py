e7b29f02816ed2b77e932169a9eb0176bd55eb2c
hadynj <hadynjiee@gmail.com>
2026-06-01 23:59:19 -0400

    Control flow

--- PythonPro/CS50s/Week1/grade.py
+++ PythonPro/CS50s/Week1/grade.py
@@ -1,7 +1,32 @@
+#and
 # convert score value to a grade letter 
 
 score = int(input("Score: "))   # ask user for grade 
 
+'''
+if score >= 90 and score <= 100:
+    print("Grade: A")
+elif score >=80 and score < 90:
+    print("Grade: B")
+elif score >=70 and score < 80:
+    print("Grade: C")
+elif score >=60 and score < 70:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+
+  if 90 <= score <= 100:
+      print("Grade: A")
+  elif 80 <= score < 90:
+      print("Grade: B")
+  elif 70 <= score < 80:
+      print("Grade: C")
+  elif 60 <= score < 70:
+      print("Grade: D")
+  else:
+      print("Grade: F")
+'''
 # check conditional and execute results 
 if score >= 90:
     print("Grade: A")
