bcd20db82431f8ef2bf06f44f3fe3eebb785e944
hadynj <hadynjiee@gmail.com>
2026-06-04 23:59:20 -0400

    Conditionals

--- PythonPro/CS50s/Week1/WK1Ex/deep.py
+++ PythonPro/CS50s/Week1/WK1Ex/deep.py
@@ -1,4 +1,4 @@
-question = input("Enter Great Question of Life? ").casefold().strip()
+question = input("What is the Answer to the Great Questioln of Life? ").casefold().strip()
 
 if question == "42" or int(42) or "forty-two" or "fourty two":
     print("Yes")
