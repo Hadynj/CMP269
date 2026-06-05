5cc7c7b37d7afb9d78551ce8ea590ce5a7bd1ca3
hadynj <hadynjiee@gmail.com>
2026-06-05 00:12:21 -0400

    Conditionals

--- PythonPro/CS50s/Week1/WK1Ex/deep.py
+++ PythonPro/CS50s/Week1/WK1Ex/deep.py
@@ -1,6 +1,14 @@
+''' Goal: implements a program that prompts the user 
+for the answer to the Great Question of Life, the Universe 
+and Everything, outputting Yes if the user inputs 
+42 or (case-insensitively)forty-two or forty two. 
+Otherwise output No '''
+
+# prompt user for input
 question = input("What is the Answer to the Great Questioln of Life? ").casefold().strip()
 
-if question == "42" or int(42) or "forty-two" or "fourty two":
+# check for answer 
+if question == ("42" or "forty-two" or "fourty two"):
     print("Yes")
 else:
-    print("no")
+    print("No")
