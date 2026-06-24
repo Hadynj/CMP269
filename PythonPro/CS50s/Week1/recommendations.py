10a9db459da02e0e15a8ca278894a4cd0f4c30d2
hadynj <hadynjiee@gmail.com>
2026-06-24 02:53:33 -0400

    Conditionals

---
+++ PythonPro/CS50s/Week1/recommendations.py
@@ -0,0 +1,15 @@
+# recommend card games to users 
+
+def main():
+    difficulty = input("Difficult or Casual? ")
+    players = input("Multiplayer or Single-player? ")
+    if difficulty == "Difficult":
+        ...
+
+
+def recommned(game):
+    print("You might like", game)
+    
+
+main()
+
