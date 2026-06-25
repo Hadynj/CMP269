57ae9d7af0b5ea37097205f56d2004c8664f1359
hadynj <hadynjiee@gmail.com>
2026-06-25 08:32:30 -0400

    Conditionals

---
+++ PythonPro/CS50s/Week1/recommend.py
@@ -0,0 +1,17 @@
+def main():
+    difficulty = input("Difficult or Casual? ")
+
+    if not (difficulty == "Difficult" or difficulty == "Casual"):
+        print("Enter a vality difficulty")
+        return
+
+    players = input("Multiplayer or Single-player? ")
+    if not (players == "Multiplayer" or players == "Single-player"):
+        print("Enter a valid number of players")
+        return
+
+    
+
+
+
+
